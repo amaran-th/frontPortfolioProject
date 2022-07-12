942a60b27b3ea6f9e3f3cc003584943eab2f59b2
llddang <bodyness@naver.com>
2022-07-13 02:02:59 +0900

    llddang) 기본적인 화면 구성 + 1주차 (로딩화면)
    - 초기 디자인

--- src/page/Lee/Lee.jsx
+++ src/page/Lee/Lee.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+
+// local
+import Week1 from "./projects/week1/Loading";
+import Note1 from "./projects/week1/Note";
+import Week2 from "./projects/week2/Test";
+import Note2 from "./projects/week2/TestNote";
+
+const pages = [0, 1];
+const projects = [<Week1 />, <Week2 />];
+const notes = [<Note1 />, <Note2 />];
 
 const Lee = () => {
-  return <div>이다은의 페이지 입니다!</div>;
+  const [page, setPage] = useState(<Week1 />);
+  const [note, setNote] = useState(<Note1 />);
+
+  const onclick = (idx) => {
+    setPage(projects[idx]);
+    setNote(notes[idx]);
+  };
+
+  return (
+    <div className="h-screen bg-[#aaddaa] flex justify-center">
+      <div className="w-2/3 flex flex-col mt-12 justify-start">
+        <div className="flex flex-row">
+          {pages.map((idx) => (
+            <div
+              className="py-1 px-4 w-fit bg-white rounded-t-md text-center text-[#609060] text-lg"
+              onClick={() => {
+                onclick(idx);
+              }}
+            >
+              {idx + 1}주차
+            </div>
+          ))}
+        </div>
+
+        <div className="w-full h-2/3 bg-white rounded-b-lg rounded-r-lg flex flex-row">
+          <div className="w-1/2 h-wrap m-3 bg-[#5a7a5a] rounded-md flex items-center justify-center">
+            {page}
+          </div>
+          <div className="w-1/2 h-wrap m-3 bg-[#5a7a5a] rounded-md flex justify-center">
+            {note}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Lee;
