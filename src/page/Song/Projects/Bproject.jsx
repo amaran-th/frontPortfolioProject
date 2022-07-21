71527c1a0466cddb617fc2f25da61eddbd339313
amaran-th <songsy405@gmail.com>
2022-07-21 20:28:07 +0900

    2주차 설명 추가

--- src/page/Song/Projects/Bproject.jsx
+++ src/page/Song/Projects/Bproject.jsx
@@ -19,7 +19,11 @@ const Bproject = () => {
           <span></span>
           <span></span>
         </div>
-        <div className="description">2주차</div>
+        <div className="description">
+          9개의 정육면체 객체를 만들고, 중앙의 객체가 떠올랐다 가라앉는동안
+          나머지 8개의 객체가 각자 <strong>딜레이</strong>를 가지고 낮게
+          떠올랐다 가라앉는 로딩 애니메이션
+        </div>
       </div>
     </>
   )
