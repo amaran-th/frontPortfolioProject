02c88b5682130e0599cf4e9c0fec936f8e7d1e08
amaran-th <songsy405@gmail.com>
2022-10-06 01:59:24 +0900

    자동뱊포 test

--- src/page/Song/Song.jsx
+++ src/page/Song/Song.jsx
@@ -47,7 +47,7 @@ const Song = () => {
   return (
     <>
       <div className="rootDiv">
-        {/**console.log('test') */}
+        {/**console.log('tㅇㅇㅇest') */}
         <div className="navigation">
           <div id="rotateNav">
             <ul>
